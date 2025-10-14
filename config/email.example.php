a6aa6b887d0257606079efb138261b006525f567
Stacey Boyer <cptr13@gmail.com>
2025-10-13 23:04:36 -0400

    Create email.example.php
    Email settings template.  Rename to email.php

---
+++ config/email.example.php
@@ -0,0 +1,14 @@
+<?php
+return array (
+  'smtp_enabled'   => true,
+  'from_email'     => 'you@example.com',
+  'from_name'      => 'Your Name or Company',
+  'smtp_host'      => 'smtp.example.com',
+  'smtp_port'      => 587,
+  'encryption'     => 'starttls',  // options: none | starttls | smtps
+  'username'       => 'you@example.com',
+  'password'       => 'your_email_password',
+  'reply_to_email' => 'you@example.com',
+  'reply_to_name'  => 'Your Name',
+  'timeout'        => 25,
+);
